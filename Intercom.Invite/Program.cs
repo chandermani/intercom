18cb1b0ddc348b6a5d08b0765ce138a97832a2a8
chandermani <chandermani@outlook.com>
2020-12-21 17:53:47 +0000

    Added code to run the code form console and generate output file.
    Also added generated output file for test input file.

--- Intercom.Invite/Program.cs
+++ Intercom.Invite/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
 
 namespace Intercom.Invite
 {
@@ -6,6 +9,36 @@ namespace Intercom.Invite
     {
         static void Main(string[] args)
         {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Incorrect number of arguments provided!");
+                Console.WriteLine("From Intecom.Invite folder RUN: dotnet run <inputfilepath> <outputfilepath>");
+                return;
+            }
+
+            string inputFilePath = args[0];
+            string outputFilePath = args[1];
+
+            if (!File.Exists(inputFilePath))
+            {
+                Console.WriteLine($"Cannot find file: {inputFilePath}");
+                return;
+            }
+
+            CustomerListFilter customerListFilter =
+                new CustomerListFilter(new CustomerListReader(inputFilePath, new CustomerModelBuilder()),
+                                        DistanceCalculator.GreatCircleDistance);
+
+            GPSCoordinate dublinCoordinates = new GPSCoordinate(53.339428, -6.257664);
+            double radius = 100d;
+
+            using (StreamWriter writer = new StreamWriter(File.OpenWrite(outputFilePath)))
+            {
+                foreach(var customer in customerListFilter.Filter(dublinCoordinates, radius).OrderBy(c=>c.Id))
+                {
+                    writer.WriteLine(JsonConvert.SerializeObject(new { user_id = customer.Id, name = customer.Name }));
+                }
+            }
         }
     }
 }
